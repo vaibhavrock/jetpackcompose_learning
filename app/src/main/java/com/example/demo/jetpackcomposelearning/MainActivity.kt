9b39f776cc588fed0e036d76d347de88301d8904
vaibhavrock <vaibhavupadhyay28@gmail.com>
2024-03-14 17:26:27 +0530

    Task-2 has been done, adding images top of text

--- app/src/main/java/com/example/demo/jetpackcomposelearning/MainActivity.kt
+++ app/src/main/java/com/example/demo/jetpackcomposelearning/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.example.demo.jetpackcomposelearning
 
 import android.os.Bundle
-import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.demo.jetpackcomposelearning.ui.theme.JetpackComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,16 +39,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Box(contentAlignment = Alignment.Center) {
-        Text(
-            textAlign = TextAlign.Center,
-            text = "Hello $name!",
-            modifier = modifier
-        )
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+        modifier = modifier.padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.ic_launcher_foreground),
+                contentDescription = "My first image",
+            )
+            Text(
+                text = "Hello $name!",
+                fontSize = 24.sp,
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    //If true, the @Composable will use a default background color.
+    showBackground = true,
+    //If true, the status bar and action bar of the device will be displayed. The @Composable will be render in the context of a full activity.
+    showSystemUi = true
+)
 @Composable
 fun GreetingPreview() {
     JetpackComposeLearningTheme {

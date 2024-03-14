be9fc4d578ae480a6b4c32a078d594e0bc8bb460
vaibhavrock <vaibhavupadhyay28@gmail.com>
2024-03-14 13:41:46 +0530

    task-1 Print hello in center has been completed

---
+++ app/src/main/java/com/example/demo/jetpackcomposelearning/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.demo.jetpackcomposelearning
+
+import android.os.Bundle
+import android.widget.RelativeLayout
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.demo.jetpackcomposelearning.ui.theme.JetpackComposeLearningTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeLearningTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("World")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(contentAlignment = Alignment.Center) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeLearningTheme {
+        Greeting("World")
+    }
+}

e8f947b790102231b62e7f74a26cc6e845b5549b
vaibhavrock <vaibhavupadhyay28@gmail.com>
2024-03-16 19:00:34 +0530

    Task 3: Below the text, add a Login button that shows a toast when clicked.

--- app/src/main/java/com/example/demo/jetpackcomposelearning/MainActivity.kt
+++ app/src/main/java/com/example/demo/jetpackcomposelearning/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.demo.jetpackcomposelearning
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,25 +50,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var showToast by remember { mutableStateOf(false) }
     Box(
         contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp)
     ) {
-        Image(
-            painter = painterResource(
-                id = R.drawable.ic_launcher_foreground
-            ),
-            contentDescription = "My first image",
-            modifier = Modifier.align(Alignment.TopCenter)
-        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .border(2.dp, Color.DarkGray)
+                .background(Color.LightGray)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "My first image",
+                modifier = Modifier.padding(8.dp)
+            )
+        }
         Column(
-        modifier = modifier.padding(8.dp)
+              modifier = modifier.padding(8.dp),
+              //verticalArrangement = Arrangement.Center,
+              horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = "Hello $name!",
                 fontSize = 24.sp,
             )
+            Button(onClick = { showToast = true }) {
+                Text("Login")
+            }
         }
+    }
+    if (showToast) {
+        showToast = false
+        val context = LocalContext.current
+        Toast.makeText(context, "Button clicked!", Toast.LENGTH_SHORT).show()
     }
 }
 
